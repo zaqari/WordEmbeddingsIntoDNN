4c687ca1d981ba4b65cef7623d34ff659f3d1f29
Zachary P Rosen <zachary.p.rosen@colorado.edu>
2017-06-12 11:14:48 -0600

    Create Embeddings_2_DNNClass_General.py
    less cursing, cleaned up build_DNNarray(), and described the functional blocks in the code better. Made it possible to run evaluations of the model if you've built test data of some sort via entering

--- Embeddings_2_DNNClass_General.py
+++ Embeddings_2_DNNClass_General.py
@@ -16,30 +16,33 @@ import csv
 import tensorflow as tf
 
 #In order to build a tag-embedding layer for EVERYTHING.
-readfile = input('Where are your fucking morphemes and tags, fuckface???  ')
+# Works by formatting input from a separate document of morpheme or word glosses
+# compiled by the researcher, and then using the gloss data as the precise label
+# used in classifying the vectors from there.
+# --Begin Code--
+readfile = input('Where is your gloss data coming from?  ')
 readinto_COLUMNS = ['word', 'tags']
 df_readinto = pd.read_csv(readfile, names=readinto_COLUMNS, skipinitialspace=True)
 NCLASSES = len(df_readinto['tags']) + 1
-print(NCLASSES)
-
-#variables
-Word2Vecmodel = input('Where are your word embeddings coming from, shitbags? ')
-training_data_location = input('Where is your training data going, goat-gargler?? ')
-
+#Inputting the variables from the pre-made word embedding vectors
+Word2Vecmodel = input('Where is your word embedding vector model coming from?  ')
+training_data_location = input('where are you putting your training data (remember to give the file a name as well, or if you\'re adding new data to an old file, to indicate the old file name)?  ')
 model = models.Word2Vec.load(Word2Vecmodel)
 word = model.wv.vocab
 
 #Just some notes when the program is run/
-print('Make sure to (1) run build_DNNarray(\'the lexeme you\'re interested in\'), and then (2) run_rabbit_run()')
+print('Make sure to (1) run build_DNNarray(), and then (2) run_rabbit_run()')
 
-#Pulls data from the corpus and formats it
-def build_DNNarray(WORD=word, MODEL=model):
+#Pulls data from the corpus of word embedding vectors and transforms it into
+# a row in a CSV that the machine learning model can use to make predictions of
+# some sort.
+def build_DNNarray(MODEL=model):
     array = []
     label_values = list(df_readinto['tags'].values)
     vec_values = list(df_readinto['word'].values)
     with codecs.open(training_data_location, 'a', 'utf-8') as csvfile:
         databuilder = csv.writer(csvfile, delimiter=',',
-                                 quotechar='|',
+                                 quotechar='',
                                  quoting=csv.QUOTE_MINIMAL)
         for item in label_values:
                 array.append(list(model[vec_values[label_values.index(item)]]))
@@ -49,8 +52,9 @@ def build_DNNarray(WORD=word, MODEL=model):
     csvfile.close()
 
 
-#Components for the DNN. Since we're playing with vectors,
-#I ended up de-activating sections relating to categorical columns--they weren't necessary.
+#Components for the DNN. Since we're playing with vectors, I ended up
+# de-activating sections relating to categorical columns--they weren't
+# necessary.
 COLUMNS = list(range(100)) + ['tag1']
 LABEL_COLUMN = inty = tf.contrib.layers.sparse_column_with_hash_bucket('tag1', hash_bucket_size=int(1000), dtype=tf.string)
 CONTINUOUS_COLUMNS = list(range(100))
@@ -93,7 +97,7 @@ def make_features(columns=CONTINUOUS_COLUMNS):
 
 #The following two lists are place-holders prior to running actual model in run_rabbit_run()
 wide_columns=[0]
-deep_columns=[]
+deep_columns=[0]
 
 #The actual model.
 m = tf.contrib.learn.DNNLinearCombinedClassifier(
@@ -103,16 +107,23 @@ m = tf.contrib.learn.DNNLinearCombinedClassifier(
     dnn_hidden_units=[100],
     n_classes=NCLASSES)
 
-#run this to put everything together after you've built some training data.
-def run_rabbit_run():
+#Run this to put everything together after you've built some training data.
+# If you want to run this as a DNN with x-number of layers, change the variable
+# for dnn_hidden_units in the variable 'm' right about this function, and switch
+# the variable deep_columns to be equal to feautures, and wide_columns to be
+# equal to [] in run_rabbit_run(). If you want to evaluate the model, run 
+# run_rabbit_run(True) to see evauative statistics of the model. Test data needs
+# be built manually, however. There is no function in the script to build it for you.
+def run_rabbit_run(Eval=False):
     make_features()
     wide_columns = features
     deep_columns = []
-    df_train = pd.read_csv(training_data_location, names=COLUMNS, skipinitialspace=True))
-    #df_test = pd.read_csv('/Users/ZaqRosen/Desktop/ARAPAHO_test_data.csv', names=COLUMNS, skipinitialspace=True, skiprows=1))
+    df_train = pd.read_csv(training_data_location, names=COLUMNS, skipinitialspace=True)
     wide_collumns = make_features()
     m.fit(input_fn=train_input_fn, steps=2000)
-    #results = m.evaluate(input_fn=eval_input_fn, steps=20)
-    #print(results)
-    var = tf.trainable_variables()
-    print(var)
+    if Eval=True:
+        df_test = pd.read_csv(input('Where is your test data coming from?  '), names=COLUMNS, skipinitialspace=True, skiprows=1)
+        results = m.evaluate(input_fn=eval_input_fn, steps=20)
+        print(results)
+        #var = tf.trainable_variables()
+        #print(var)
